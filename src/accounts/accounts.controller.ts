e7b0ff5bbed5e73621337637c782f6b09a831ebb
pplayer66 <lander89@inbox.ru>
2022-03-16 17:03:42 +0600

    accounts.json

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from "@nestjs/common";
+import { readFile } from "fs/promises";
 
 @Controller("accounts")
 export class AccountsController {
   @Get()
-  getAccounts() {
-    return "accounts";
+  async getAccounts() {
+    const accounts = await readFile("accounts.json", "utf8");
+    return JSON.parse(accounts);
   }
 }
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,28 +1,26 @@
 import {
   Body,
   Controller,
-  Get,
-  Param,
-  Post,
   HttpException,
   HttpStatus,
-} from '@nestjs/common';
-import { readFile } from 'fs/promises';
-import { AuthDto } from './dtos/auth.dto';
+  Post,
+} from "@nestjs/common";
+import { readFile } from "fs/promises";
+import { AuthDto } from "./dtos/auth.dto";
 
-@Controller('auth')
+@Controller("auth")
 export class AuthController {
   @Post()
   async login(@Body() body: AuthDto) {
     const { login, password } = body;
-    if (login === 'fred@gmail.com' && password === '123') {
-      const data = await readFile('user.json', 'utf8');
+    if (login === "fred@gmail.com" && password === "123") {
+      const data = await readFile("user.json", "utf8");
 
       const userData = JSON.parse(data);
       return userData[login];
     }
     throw new HttpException(
-      'Invalid username or password',
+      "Invalid username or password",
       HttpStatus.BAD_REQUEST
     );
   }
