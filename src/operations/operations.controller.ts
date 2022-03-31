9f58b03937a0cf77d0baf93bb38c986965d69027
pplayer66 <lander89@inbox.ru>
2022-03-31 15:54:28 +0600

    operations

--- src/operations/operations.controller.ts
+++ src/operations/operations.controller.ts
@@ -1,16 +1,26 @@
-import { Controller, Get, Query } from "@nestjs/common";
-import { readFile } from "fs/promises";
-import * as moment from "moment";
+import { Controller, Get, Query } from '@nestjs/common';
+import { readFile } from 'fs/promises';
+import * as moment from 'moment';
 
-@Controller("operations")
+@Controller('operations')
 export class OperationsController {
-  @Get()
-  async getOperations(@Query("last") last: string) {
-    const operationsJson = await readFile("operations.json", "utf8");
+  @Get('/last')
+  async getLastOperations() {
+    const operationsJson = await readFile('operations.json', 'utf8');
     const operations = JSON.parse(operationsJson);
     const sortedOperations = operations.sort((a, b) =>
       moment(a.date).isBefore(moment(b.date)) ? 1 : -1
     );
     return sortedOperations.slice(0, 3);
+  }
+
+  @Get()
+  async getOperations() {
+    const operationsJson = await readFile('operations.json', 'utf8');
+    const operations = JSON.parse(operationsJson);
+    const sortedOperations = operations.sort((a, b) =>
+      moment(a.date).isBefore(moment(b.date)) ? 1 : -1
+    );
+    return sortedOperations;
   }
 }
