c790ed23f50bdbf1d2d110bd0ee467246b6298e5
pplayer66 <lander89@inbox.ru>
2022-04-15 16:25:40 +0600

    .

--- src/transfers/transfers.controller.ts
+++ src/transfers/transfers.controller.ts
@@ -1,36 +1,40 @@
-import { Body, Controller, Post } from "@nestjs/common";
-import { readFile, writeFile } from "fs/promises";
+import { Body, Controller, Post } from '@nestjs/common';
+import { readFile, writeFile } from 'fs/promises';
 
-@Controller("transfers")
+@Controller('transfers')
 export class TransfersController {
-  @Post("/proceed")
+  @Post('/proceed')
   async transfer(@Body() body: any) {
     const { amount, account, transferTo, transferId } = body;
 
-    const accountsJson = await readFile("accounts.json", "utf8");
+    const accountsJson = await readFile('accounts.json', 'utf8');
     const accounts = JSON.parse(accountsJson);
-    const accountFrom = accounts.find((acc) => acc.id == account);
+    const accountFrom = accounts.find(acc => acc.id == account);
     const newAccount = {
       ...accountFrom,
       balance: accountFrom.balance - amount,
     };
     console.log(newAccount);
-    let newAccounts = accounts.map((acc) =>
+    let newAccounts = accounts.map(acc =>
       acc.id == newAccount.id ? newAccount : acc
     );
 
-    if (transferId === "internal_transfer") {
-      const accountTo = newAccounts.find((acc) => acc.id == transferTo);
+    if (transferId === 'internal_transfer') {
+      const accountTo = newAccounts.find(acc => acc.id == transferTo);
       console.log(accountTo);
       const newAccountTo = {
         ...accountTo,
         balance: +accountTo.balance + amount,
       };
       console.log(newAccountTo);
-      newAccounts = newAccounts.map((acc) =>
+      newAccounts = newAccounts.map(acc =>
         acc.id == newAccountTo.id ? newAccountTo : acc
       );
     }
-    await writeFile("accounts.json", JSON.stringify(newAccounts));
+
+    return await new Promise(async resolve => {
+      await writeFile('accounts.json', JSON.stringify(newAccounts));
+      setTimeout(() => resolve(newAccounts), 2000);
+    });
   }
 }
