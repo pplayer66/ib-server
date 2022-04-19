f9fbdc3896f5d6ac551fe9b99327d2812ca428f5
pplayer66 <lander89@inbox.ru>
2022-04-19 18:36:05 +0600

    .

--- src/conversion/conversion.controller.ts
+++ src/conversion/conversion.controller.ts
@@ -1,10 +1,9 @@
 import { Body, Controller, Post } from "@nestjs/common";
-import { readFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
 
-const ratesMap = {
-  USD: 453,
-  RUB: 5.46,
-};
+const usdToKzt = 453;
+const rubToKzt = 5.43;
+const ustToRub = 79;
 
 @Controller("conversion")
 export class ConversionController {
@@ -13,8 +12,37 @@ export class ConversionController {
     const { amount, account, transferTo, transferId } = body;
 
     const accountsJson = await readFile("accounts.json", "utf8");
-    const accounts = JSON.parse(accountsJson);
+    let accounts = JSON.parse(accountsJson);
     const accountFrom = accounts.find((acc) => acc.id == account);
+    accountFrom.balance = accountFrom.balance - amount;
+    const fromCur = accountFrom.currencyCode;
+
     const accountTo = accounts.find((acc) => acc.id == transferTo);
+    let balanceTo = accountTo.balance;
+    const curTo = accountTo.currencyCode;
+
+    if (fromCur === "USD" && curTo === "RUB") {
+      balanceTo = balanceTo + amount * 79;
+    } else if (fromCur === "RUB" && curTo === "USD") {
+      balanceTo = balanceTo + amount / 79;
+    } else if (fromCur === "USD" && curTo === "KZT") {
+      balanceTo = balanceTo + amount * 453;
+    } else if (fromCur === "KZT" && curTo === "USD") {
+      balanceTo = balanceTo + amount / 453;
+    } else if (fromCur === "RUB" && curTo === "KZT") {
+      balanceTo = balanceTo + amount * 5.43;
+    } else if (fromCur === "KZT" && curTo === "RUB") {
+      balanceTo = balanceTo + amount / 5.43;
+    }
+
+    accountTo.balance = balanceTo;
+
+    accounts = accounts.map((acc) => {
+      if (acc.id == account) return accountFrom;
+      if (acc.id == transferTo) return transferTo;
+      return acc;
+    });
+
+    await writeFile("accounts.json", JSON.stringify(accounts));
   }
 }
